515c8fa132146ac2bc96b445cee6e7f597211473
이정훈 <eu4ng97@gmail.com>
2026-04-29 08:55:55 +0900

    환경 변수가 변경될 경우를 고려하여 재시작할 때마다 재등록하도록 변경

--- scripts/register.sh
+++ scripts/register.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-if [ ! -f /etc/gitlab-runner/config.toml ]; then
-    echo 'Registering new GitLab Runner...'
+echo 'Registering new GitLab Runner...'
 
-    gitlab-runner register \
+gitlab-runner register \
     --non-interactive \
     --url $GITLAB_URL \
     --token $(cat /run/secrets/token) \
     --executor $EXECUTOR \
     --docker-image $DOCKER_IMAGE
 
-    sed -i "s/^concurrent = .*/concurrent = $CONCURRENT/" /etc/gitlab-runner/config.toml
-else
-    echo 'Runner is already registered.'
-fi
+sed -i "s/^concurrent = .*/concurrent = $CONCURRENT/" /etc/gitlab-runner/config.toml
+sed -i "s/^request_concurrency = .*/request_concurrency = $CONCURRENT/" /etc/gitlab-runner/config.toml
