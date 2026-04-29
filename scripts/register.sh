2bb655fa00ef33d58859fba8b7893ba168612924
이정훈 <eu4ng97@gmail.com>
2026-04-29 09:04:41 +0900

    request_concurrency 예외 처리 추가

--- scripts/register.sh
+++ scripts/register.sh
@@ -9,5 +9,12 @@ gitlab-runner register \
     --executor $EXECUTOR \
     --docker-image $DOCKER_IMAGE
 
+# concurrent 설정
 sed -i "s/^concurrent = .*/concurrent = $CONCURRENT/" /etc/gitlab-runner/config.toml
-sed -i "s/^request_concurrency = .*/request_concurrency = $CONCURRENT/" /etc/gitlab-runner/config.toml
+
+# request_concurrency 설정
+if grep -q "request_concurrency" /etc/gitlab-runner/config.toml; then
+    sed -i "s|^request_concurrency = .*|request_concurrency = $CONCURRENT|" /etc/gitlab-runner/config.toml
+else
+    sed -i "2i request_concurrency = $CONCURRENT" /etc/gitlab-runner/config.toml
+fi
